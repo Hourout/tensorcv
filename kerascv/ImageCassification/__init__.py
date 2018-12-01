c3caa3c471fd8b8e43b193386b962b8119c9babc
JinQing Lee <hourout@163.com>
2018-12-01 15:42:36 +0800

    Update __init__.py

--- kerascv/ImageCassification/__init__.py
+++ kerascv/ImageCassification/__init__.py
@@ -1,9 +1,8 @@
 from kerascv.ImageCassification._alexnet import alexnet
-from kerascv.ImageCassification._vgg import vgg11
-from kerascv.ImageCassification._vgg import vgg11_bn
-from kerascv.ImageCassification._vgg import vgg13
-from kerascv.ImageCassification._vgg import vgg13_bn
-from kerascv.ImageCassification._vgg import vgg16
-from kerascv.ImageCassification._vgg import vgg16_bn
-from kerascv.ImageCassification._vgg import vgg19
-from kerascv.ImageCassification._vgg import vgg19_bn
+
+from kerascv.ImageCassification._vgg import vgg11, vgg11_bn
+from kerascv.ImageCassification._vgg import vgg13, vgg13_bn
+from kerascv.ImageCassification._vgg import vgg16, vgg16_bn
+from kerascv.ImageCassification._vgg import vgg19, vgg19_bn
+
+from kerascv.ImageCassification._densenet import densenet121, densenet161, densenet169, densenet201
