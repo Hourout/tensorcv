c5b5972ea94b3ede21ffff120afd7f55313f3036
JinQing Lee <hourout@163.com>
2018-12-01 16:03:32 +0800

    add densnet

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='kerascv',
-      version='0.0.1',
+      version='0.0.2',
       install_requires=['tensorflow>=1.12.0'],
       description='tf.Keras implementations of a Deep Learning Toolkit for Computer Vision.',
       long_description=readme(),
