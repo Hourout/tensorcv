79cc66663ed927c7e6a63fa71471086dfb2917dc
JinQing Lee <hourout@163.com>
2018-12-01 23:11:27 +0800

    add senet

--- kerascv/ImageCassification/__init__.py
+++ kerascv/ImageCassification/__init__.py
@@ -6,3 +6,5 @@ from kerascv.ImageCassification._vgg import vgg16, vgg16_bn
 from kerascv.ImageCassification._vgg import vgg19, vgg19_bn
 
 from kerascv.ImageCassification._densenet import densenet121, densenet161, densenet169, densenet201
+
+from kerascv.ImageCassification._senet import senet_50, senet_101, senet_152
