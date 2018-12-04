c23b0072f127049e071d0836ac87ff164e1cc793
JinQing Lee <hourout@163.com>
2018-12-04 17:18:01 +0800

    remove layer name

--- kerascv/ImageCassification/_alexnet.py
+++ kerascv/ImageCassification/_alexnet.py
@@ -2,25 +2,34 @@ import tensorflow as tf
 
 alexnet_url = None
 
-def alexnet(input_shape, include_top=True, pretrain_file=False, classes=1000):
+def Alexnet(tensor):
+    x = tf.keras.layers.ZeroPadding2D(2)(tensor)
+    x = tf.keras.layers.Conv2D(64, 11, 4, activation='relu')(x)
+    x = tf.keras.layers.MaxPool2D(3, 2)(x)
+    x = tf.keras.layers.ZeroPadding2D(2)(x)
+    x = tf.keras.layers.Conv2D(192, 5, activation='relu')(x)
+    x = tf.keras.layers.MaxPool2D(3, 2)(x)
+    x = tf.keras.layers.ZeroPadding2D(1)(x)
+    x = tf.keras.layers.Conv2D(384, 3, activation='relu')(x)
+    x = tf.keras.layers.ZeroPadding2D(1)(x)
+    x = tf.keras.layers.Conv2D(256, 3, activation='relu')(x)
+    x = tf.keras.layers.ZeroPadding2D(1)(x)
+    x = tf.keras.layers.Conv2D(256, 3, activation='relu')(x)
+    x = tf.keras.layers.MaxPool2D(3, 2)(x)
+    return x
+
+def get_alexnet(input_shape, include_top, pretrain_file, classes):
     if isinstance(pretrain_file, str) and include_top and classes != 1000:
         raise ValueError('If using `pretrain weights` with `include_top` as true, `classes` should be 1000')
     image = tf.keras.Input(shape=input_shape)
-    x = tf.keras.layers.Conv2D(64, 11, 4, 'same', activation='relu', name='conv1')(image)
-    x = tf.keras.layers.MaxPool2D(3, 2, name='pool1')(x)
-    x = tf.keras.layers.Conv2D(192, 5, 1, 'same', activation='relu', name='conv2')(x)
-    x = tf.keras.layers.MaxPool2D(3, 2, name='pool2')(x)
-    x = tf.keras.layers.Conv2D(384, 3, 1, 'same', activation='relu', name='conv3')(x)
-    x = tf.keras.layers.Conv2D(256, 3, 1, 'same', activation='relu', name='conv4')(x)
-    x = tf.keras.layers.Conv2D(256, 3, 1, 'same', activation='relu', name='conv5')(x)
+    x = Alexnet(image)
     if include_top:
-        x = tf.keras.layers.MaxPool2D(3, 2, name='pool3')(x)
-        x = tf.keras.layers.Flatten(name='flatten')(x)
-        x = tf.keras.layers.Dense(4096, activation='relu', name='liner1')(x)
-        x = tf.keras.layers.Dropout(0.5, name='drop1')(x)
-        x = tf.keras.layers.Dense(4096, activation='relu', name='liner2')(x)
-        x = tf.keras.layers.Dropout(0.5, name='drop1')(x)
-        x = tf.keras.layers.Dense(classes, name='predictions')(x)
+        x = tf.keras.layers.Flatten()(x)
+        x = tf.keras.layers.Dense(4096, activation='relu')(x)
+        x = tf.keras.layers.Dropout(0.5)(x)
+        x = tf.keras.layers.Dense(4096, activation='relu')(x)
+        x = tf.keras.layers.Dropout(0.5)(x)
+        x = tf.keras.layers.Dense(classes)(x)
     model = tf.keras.Model(image, x, name='alexnet')
     if isinstance(pretrain_file, str):
         if tf.gfile.Exists(pretrain_file):
@@ -31,3 +40,6 @@ def alexnet(input_shape, include_top=True, pretrain_file=False, classes=1000):
             tf.keras.utils.get_file(pretrain_file, alexnet_url)
             model.load_weights(pretrain_file, by_name=True)
     return model
+
+def alexnet(input_shape, include_top=True, pretrain_file=False, classes=1000):
+    return get_alexnet(input_shape, include_top, pretrain_file, classes)
