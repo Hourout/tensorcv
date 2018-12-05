85b7aea4f07fd144b75eb136297c564889d94ded
JinQing Lee <hourout@163.com>
2018-12-05 20:07:06 +0800

    update library  name

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ def readme():
     with open('README.md', encoding='utf-8') as f:
         return f.read()
 
-setup(name='kerascv',
+setup(name='tensorcv',
       version='0.0.5',
       install_requires=['tensorflow>=1.12.0'],
       description='tf.Keras implementations of a Deep Learning Toolkit for Computer Vision.',
       long_description=readme(),
       long_description_content_type='text/markdown',
-      url='https://github.com/Hourout/keras-cv',
+      url='https://github.com/Hourout/tensorcv',
       author='JinQing Lee',
       author_email='hourout@163.com',
       keywords=['computer-vision', 'keras', 'tf.keras', 'deep-learning'],
-      license='Apache-2.0',
+      license='Apache License Version 2.0',
       classifiers=[
           'Intended Audience :: Developers',
           'Intended Audience :: Education',
@@ -23,7 +23,7 @@ setup(name='kerascv',
           'Topic :: Scientific/Engineering',
           'Topic :: Scientific/Engineering :: Artificial Intelligence',
           'Topic :: Scientific/Engineering :: Visualization',
-          'License :: OSI Approved :: Apache-2.0',
+          'License :: OSI Approved :: Apache Software License',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6'
       ],
