9a2cb2fb33f6146c2f5b14345a897e823e04a862
JinQing Lee <hourout@163.com>
2018-12-05 20:15:57 +0800

    Update and rename kerascv/ImageCassification/__init__.py to tensorcv/ImageCassification/__init__.py

---
+++ tensorcv/ImageCassification/__init__.py
@@ -0,0 +1,23 @@
+from tensorcv.ImageCassification._alexnet import alexnet
+
+from tensorcv.ImageCassification._vgg import vgg11, vgg11_bn
+from tensorcv.ImageCassification._vgg import vgg13, vgg13_bn
+from tensorcv.ImageCassification._vgg import vgg16, vgg16_bn
+from tensorcv.ImageCassification._vgg import vgg19, vgg19_bn
+
+from tensorcv.ImageCassification._densenet import densenet121, densenet161, densenet169, densenet201
+
+from tensorcv.ImageCassification._senet import senet_50, senet_101, senet_152
+
+from tensorcv.ImageCassification._inception import inception_v3
+
+from tensorcv.ImageCassification._resnet import resnet18_v1, resnet18_v2
+from tensorcv.ImageCassification._resnet import resnet34_v1, resnet34_v2
+from tensorcv.ImageCassification._resnet import resnet50_v1, resnet50_v2
+from tensorcv.ImageCassification._resnet import resnet101_v1, resnet101_v2
+from tensorcv.ImageCassification._resnet import resnet152_v1, resnet152_v2
+from tensorcv.ImageCassification._resnet import se_resnet18_v1, se_resnet18_v2
+from tensorcv.ImageCassification._resnet import se_resnet34_v1, se_resnet34_v2
+from tensorcv.ImageCassification._resnet import se_resnet50_v1, se_resnet50_v2
+from tensorcv.ImageCassification._resnet import se_resnet101_v1, se_resnet101_v2
+from tensorcv.ImageCassification._resnet import se_resnet152_v1, se_resnet152_v2
