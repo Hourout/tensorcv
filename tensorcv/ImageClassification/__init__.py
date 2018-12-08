3987e18d593a9650242ab60493f18b5934a0ef76
JinQing Lee <hourout@163.com>
2018-12-08 20:23:52 +0800

    Update and rename tensorcv/Classification/__init__.py to tensorcv/ImageClassification/__init__.py

--- tensorcv/ImageClassification/__init__.py
+++ tensorcv/ImageClassification/__init__.py
@@ -1,36 +1,36 @@
-from tensorcv.ImageClassification._alexnet import alexnet
+from tensorcv.Classification._alexnet import alexnet
 
-from tensorcv.ImageClassification._vgg import vgg11, vgg11_bn
-from tensorcv.ImageClassification._vgg import vgg13, vgg13_bn
-from tensorcv.ImageClassification._vgg import vgg16, vgg16_bn
-from tensorcv.ImageClassification._vgg import vgg19, vgg19_bn
+from tensorcv.Classification._vgg import vgg11, vgg11_bn
+from tensorcv.Classification._vgg import vgg13, vgg13_bn
+from tensorcv.Classification._vgg import vgg16, vgg16_bn
+from tensorcv.Classification._vgg import vgg19, vgg19_bn
 
-from tensorcv.ImageClassification._densenet import densenet121, densenet161, densenet169, densenet201
+from tensorcv.Classification._densenet import densenet121, densenet161, densenet169, densenet201
 
-from tensorcv.ImageClassification._senet import senet_50, senet_101, senet_152
+from tensorcv.Classification._senet import senet_50, senet_101, senet_152
 
-from tensorcv.ImageClassification._inception import inception_v3
+from tensorcv.Classification._inception import inception_v3
 
-from tensorcv.ImageClassification._resnet import resnet18_v1, resnet18_v2
-from tensorcv.ImageClassification._resnet import resnet34_v1, resnet34_v2
-from tensorcv.ImageClassification._resnet import resnet50_v1, resnet50_v2
-from tensorcv.ImageClassification._resnet import resnet101_v1, resnet101_v2
-from tensorcv.ImageClassification._resnet import resnet152_v1, resnet152_v2
-from tensorcv.ImageClassification._resnet import se_resnet18_v1, se_resnet18_v2
-from tensorcv.ImageClassification._resnet import se_resnet34_v1, se_resnet34_v2
-from tensorcv.ImageClassification._resnet import se_resnet50_v1, se_resnet50_v2
-from tensorcv.ImageClassification._resnet import se_resnet101_v1, se_resnet101_v2
-from tensorcv.ImageClassification._resnet import se_resnet152_v1, se_resnet152_v2
+from tensorcv.Classification._resnet import resnet18_v1, resnet18_v2
+from tensorcv.Classification._resnet import resnet34_v1, resnet34_v2
+from tensorcv.Classification._resnet import resnet50_v1, resnet50_v2
+from tensorcv.Classification._resnet import resnet101_v1, resnet101_v2
+from tensorcv.Classification._resnet import resnet152_v1, resnet152_v2
+from tensorcv.Classification._resnet import se_resnet18_v1, se_resnet18_v2
+from tensorcv.Classification._resnet import se_resnet34_v1, se_resnet34_v2
+from tensorcv.Classification._resnet import se_resnet50_v1, se_resnet50_v2
+from tensorcv.Classification._resnet import se_resnet101_v1, se_resnet101_v2
+from tensorcv.Classification._resnet import se_resnet152_v1, se_resnet152_v2
 
-from tensorcv.ImageClassification._squeezenet import squeezenet1_0, squeezenet1_1
+from tensorcv.Classification._squeezenet import squeezenet1_0, squeezenet1_1
 
-from tensorcv.ImageClassification._resnext import resnext50_32x4d, se_resnext50_32x4d
-from tensorcv.ImageClassification._resnext import resnext101_32x4d, se_resnext101_32x4d
-from tensorcv.ImageClassification._resnext import resnext101_64x4d, se_resnext101_64x4d
+from tensorcv.Classification._resnext import resnext50_32x4d, se_resnext50_32x4d
+from tensorcv.Classification._resnext import resnext101_32x4d, se_resnext101_32x4d
+from tensorcv.Classification._resnext import resnext101_64x4d, se_resnext101_64x4d
 
-from tensorcv.ImageClassification._resnetv1b import resnet18_v1b, resnet34_v1b
-from tensorcv.ImageClassification._resnetv1b import resnet50_v1b, resnet101_v1b, resnet152_v1b
-from tensorcv.ImageClassification._resnetv1b import resnet50_v1c, resnet101_v1c, resnet152_v1c
-from tensorcv.ImageClassification._resnetv1b import resnet50_v1d, resnet101_v1d, resnet152_v1d
-from tensorcv.ImageClassification._resnetv1b import resnet50_v1e, resnet101_v1e, resnet152_v1e
-from tensorcv.ImageClassification._resnetv1b import resnet50_v1s, resnet101_v1s, resnet152_v1s
+from tensorcv.Classification._resnetv1b import resnet18_v1b, resnet34_v1b
+from tensorcv.Classification._resnetv1b import resnet50_v1b, resnet101_v1b, resnet152_v1b
+from tensorcv.Classification._resnetv1b import resnet50_v1c, resnet101_v1c, resnet152_v1c
+from tensorcv.Classification._resnetv1b import resnet50_v1d, resnet101_v1d, resnet152_v1d
+from tensorcv.Classification._resnetv1b import resnet50_v1e, resnet101_v1e, resnet152_v1e
+from tensorcv.Classification._resnetv1b import resnet50_v1s, resnet101_v1s, resnet152_v1s
