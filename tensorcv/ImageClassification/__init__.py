0cd7f37aab10faaf49f9fbbd6aa53f20de343fbc
JinQing Lee <hourout@163.com>
2018-12-08 17:07:57 +0800

    add resnetv1b

--- tensorcv/ImageClassification/__init__.py
+++ tensorcv/ImageClassification/__init__.py
@@ -27,3 +27,9 @@ from tensorcv.ImageClassification._squeezenet import squeezenet1_0, squeezenet1_
 from tensorcv.ImageClassification._resnext import resnext50_32x4d, se_resnext50_32x4d
 from tensorcv.ImageClassification._resnext import resnext101_32x4d, se_resnext101_32x4d
 from tensorcv.ImageClassification._resnext import resnext101_64x4d, se_resnext101_64x4d
+
+from tensorcv.ImageClassification._resnetv1b import resnet18_v1b, resnet34_v1b, resnet50_v1b, resnet101_v1b, resnet152_v1b
+from tensorcv.ImageClassification._resnetv1b import resnet50_v1c, resnet101_v1c, resnet152_v1c
+from tensorcv.ImageClassification._resnetv1b import resnet50_v1d, resnet101_v1d, resnet152_v1d
+from tensorcv.ImageClassification._resnetv1b import resnet50_v1e, resnet101_v1e, resnet152_v1e
+           'resnet50_v1s', 'resnet101_v1s', 'resnet152_v1s
