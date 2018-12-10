d9b090726b9e995ad9751708fe0474757f62fa17
JinQing Lee <hourout@163.com>
2018-12-10 15:55:54 +0800

    add tc.image.read_image()

--- tensorcv/image/_image_ops.py
+++ tensorcv/image/_image_ops.py
@@ -11,3 +11,19 @@ def random_hue(image, lower, upper, seed=None):
         raise ValueError('lower and upper should be upper > lower and in the range [-1,1].')
     delta = tf.random.uniform([], lower, upper, seed=seed)
     return tf.image.adjust_hue(image, delta)
+
+def read_image(filename, channel=0, image_format='mix'):
+    image = tf.io.read_file(filename)
+    if image_format=='png':
+        image = tf.image.decode_png(image, channel)
+    elif image_format=='bmp':
+        image = tf.image.decode_bmp(image, channel)
+    elif image_format=='gif':
+        image = tf.image.decode_gif(image)
+    elif image_format in ["jpg", "jpeg"]:
+        image = tf.image.decode_jpeg(image, channel)
+    elif image_format=='mix':
+        image = tf.image.decode_image(image)
+    else:
+        raise ValueError('image_format should be one of "mix", "jpg", "jpeg", "png", "gif", "bmp".')
+    return image
