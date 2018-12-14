fe0017209c68f50dca982acda537579a47d5191d
JinQing Lee <hourout@163.com>
2018-12-14 22:48:21 +0800

    Update _image_ops.py

--- tensorcv/image/_image_ops.py
+++ tensorcv/image/_image_ops.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.python.ops.image_ops_impl import _ImageDimensions
 
 def read_image(filename, channel=0, image_format='mix'):
     """Convenience function for read image type one of `bmp`, `gif`, `jpeg`, `jpg`, and `png`.
@@ -320,7 +321,7 @@ def RandomRotation(image, k=[0, 1, 2, 3], random=True, seed=None):
         image = tf.image.rot90(image, k)
     return image
 
-def RandomCentralCropResize(image, central_rate, size, method=0, seed=None):
+def RandomCropCentralResize(image, central_rate, size, method=0, seed=None):
     """Crop the central region of the image(s) and resize specify shape.
     
     Remove the outer parts of an image but retain the central region of the image
@@ -369,7 +370,7 @@ def RandomCentralCropResize(image, central_rate, size, method=0, seed=None):
     image = tf.image.resize_images(image, size=size, method=method)
     return image
 
-def RandomPointCropResize(image, height_rate, width_rate, size, method=0, seed=None):
+def RandomCropPointResize(image, height_rate, width_rate, size, method=0, seed=None):
     """Crop the any region of the image(s) and resize specify shape.
     
     Crop region area = height_rate * width_rate *image_height * image_width
@@ -411,7 +412,26 @@ def RandomPointCropResize(image, height_rate, width_rate, size, method=0, seed=N
     return image
 
 def Normalize(image, mean=None, std=None):
-    image = tf.convert_to_tensor(image, dtype=tf.float32)
+    """Normalize scales `image` to have mean and variance.
+    
+    This op computes `(x - mean) / std`.
+    Args:
+        image: An n-D Tensor where the last 3 dimensions are
+               `[height, width, channels]`.
+        mean: if None, computes image mean.
+              if int float, customize image all channels mean.
+              if tuple list, customize image each channels mean,
+              shape should 3 dims.
+        std: if None, computes image std.
+             if int float, customize image all channels std.
+             if tuple list, customize image each channels std,
+             shape should 3 dims.
+  Returns:
+    The standardized image with same shape as `image`.
+  Raises:
+    ValueError: if the shape of 'image' is incompatible with this function.
+  """
+    image = tf.cast(image, dtype=tf.float32)
     assert image.get_shape().ndims==3, 'image ndims must be 3.'
     if mean is None and std is None:
         image = tf.image.per_image_standardization(image)
