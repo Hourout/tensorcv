eade9a0420a7c9de6e1c49939ae0e40d901f73f9
JinQing Lee <hourout@163.com>
2018-12-14 22:54:18 +0800

    Update __init__.py

--- tensorcv/image/__init__.py
+++ tensorcv/image/__init__.py
@@ -8,5 +8,11 @@ from tensorcv.image._image_ops import RandomFlipLeftRight
 from tensorcv.image._image_ops import RandomFlipTopBottom
 from tensorcv.image._image_ops import RandomTranspose
 from tensorcv.image._image_ops import RandomRotation
-from tensorcv.image._image_ops import RandomCentralCropResize
-from tensorcv.image._image_ops import RandomPointCropResize
+from tensorcv.image._image_ops import RandomCropCentralResize
+from tensorcv.image._image_ops import RandomCropPointResize
+from tensorcv.image._image_ops import Normalize
+from tensorcv.image._image_ops import RandomGaussianNoise
+from tensorcv.image._image_ops import RandomNoisePoisson
+from tensorcv.image._image_ops import RandomNoiseMask
+from tensorcv.image._image_ops import RandomNoiseSaltPepper
+from tensorcv.image._image_ops import RandomRescale
