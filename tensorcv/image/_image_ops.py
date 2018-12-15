57f0a2ba55ce4bf481eed1fb69a7952f8e47ecdc
JinQing Lee <hourout@163.com>
2018-12-15 13:49:40 +0800

    add api doc

--- tensorcv/image/_image_ops.py
+++ tensorcv/image/_image_ops.py
@@ -441,26 +441,78 @@ def Normalize(image, mean=None, std=None):
         image = tf.math.divide(tf.math.subtract(image, mean), std)
     return image
 
-def RandomGaussianNoise(image, scale=1, mean=0.0, std=1.0, seed=None):
-    assert isinstance(scale, (int, float)), 'scale type should be one of int, float.'
-    image = tf.convert_to_tensor(image, dtype=tf.float32)
-    image_shape = image.get_shape().as_list()
+def RandomRescale(image, scale, seed=None):
+    """Rescale apply to image.
+    
+    new pixel = image * scale
+    Args:
+        image: Either a 3-D float Tensor of shape [height, width, depth], or a 4-D
+               Tensor of shape [batch_size, height, width, depth].
+        scale: if int float, value multiply with image.
+               if tuple list, randomly picked in the interval
+               `[central_rate[0], central_rate[1])`, value multiply with image.
+        seed: A Python integer. Used to create a random seed.
+              See `tf.set_random_seed` for behavior.
+    Returns:
+        3-D / 4-D float Tensor, as per the input.
+    Raises:
+        scale type error.
+    """
+    if isinstance(scale, (int, float)):
+        image = tf.math.multiply(image, scale)
+    elif isinstance(scale, (tuple, list)):
+        random_scale = tf.random.uniform([], scale[0], scale[1], seed=seed)
+        image = tf.math.multiply(image, random_scale)
+    else:
+        raise ValueError('scale type should be one of int, float, tuple, list.')
+    return image
+
+def RandomNoiseGaussian(image, scale=1, mean=0.0, std=1.0, seed=None):
+    """Gaussian noise apply to image.
+    
+    new pixel = image + gaussian_noise * scale
+    Args:
+        image: Either a 3-D float Tensor of shape [height, width, depth], or a 4-D
+               Tensor of shape [batch_size, height, width, depth].
+        scale: if int or float, value multiply with poisson_noise.
+               if tuple or list, randomly picked in the interval
+               `[scale[0], scale[1])`, value multiply with poisson_noise.
+        mean: if int or float, value is gaussian distribution mean.
+              if tuple or list, randomly picked in the interval
+              `[mean[0], mean[1])`, value is gaussian distribution mean.
+        std: if int or float, value is gaussian distribution std.
+             if tuple or list, randomly picked in the interval
+             `[std[0], std[1])`, value is gaussian distribution std.
+        seed: A Python integer. Used to create a random seed.
+              See `tf.set_random_seed` for behavior.
+    Returns:
+        3-D / 4-D float Tensor, as per the input.
+    Raises:
+        scale or lam type error.
+    """
+    if isinstance(scale, (int, float)):
+        new_scale = scale
+    else:
+        assert isinstance(scale, (tuple, list)), 'scale type should be one of int, float, tuple, list.'
+        new_scale = tf.random.uniform([], scale[0], scale[1], seed=seed)
+    image = tf.cast(image, dtype=tf.float32)
+    image_shape = tf.cast(_ImageDimensions(image, image.get_shape().ndims), dtype=tf.int32)
     if isinstance(mean, (int, float)):
         if isinstance(std, (int, float)):
-            image = tf.math.add(tf.math.multiply(tf.random.normal(image_shape, mean, std, seed=seed), scale), image)
+            image = tf.math.add(tf.math.multiply(tf.random.normal(image_shape, mean, std, seed=seed), new_scale), image)
         elif isinstance(std, (tuple, list)):
             random_std = tf.random.uniform([], std[0], std[1])
-            image = tf.math.add(tf.math.multiply(tf.random.normal(image_shape, mean, random_std, seed=seed), scale), image)
+            image = tf.math.add(tf.math.multiply(tf.random.normal(image_shape, mean, random_std, seed=seed), new_scale), image)
         else:
             raise ValueError('std type should be one of int, float, tuple, list.')
     elif isinstance(mean, (tuple, list)):
         if isinstance(std, (int, float)):
             random_mean = tf.random.uniform([], mean[0], mean[1])
-            image = tf.math.add(tf.math.multiply(tf.random.normal(image_shape, random_mean, std, seed=seed), scale), image)
+            image = tf.math.add(tf.math.multiply(tf.random.normal(image_shape, random_mean, std, seed=seed), new_scale), image)
         elif isinstance(std, (tuple, list)):
             random_mean = tf.random.uniform([], mean[0], mean[1])
             random_std = tf.random.uniform([], std[0], std[1])
-            image = tf.math.add(tf.math.multiply(tf.random.normal(image_shape, random_mean, random_std, seed=seed), scale), image)
+            image = tf.math.add(tf.math.multiply(tf.random.normal(image_shape, random_mean, random_std, seed=seed), new_scale), image)
         else:
             raise ValueError('std type should be one of int, float, tuple, list.')
     else:
@@ -468,19 +520,62 @@ def RandomGaussianNoise(image, scale=1, mean=0.0, std=1.0, seed=None):
     return image
 
 def RandomNoisePoisson(image, scale=1, lam=1.0, seed=None):
-    assert isinstance(scale, (int, float)), 'scale type should be one of int, float.'
+    """Poisson noise apply to image.
+    
+    new pixel = image + poisson_noise * scale
+    Args:
+        image: Either a 3-D float Tensor of shape [height, width, depth], or a 4-D
+               Tensor of shape [batch_size, height, width, depth].
+        scale: if int or float, value multiply with poisson_noise.
+               if tuple or list, randomly picked in the interval
+               `[scale[0], scale[1])`, value multiply with poisson_noise.
+        lam: if int or float, value is poisson distribution lambda.
+             if tuple or list, randomly picked in the interval
+             `[lam[0], lam[1])`, value is poisson distribution lambda.
+        seed: A Python integer. Used to create a random seed.
+              See `tf.set_random_seed` for behavior.
+    Returns:
+        3-D / 4-D float Tensor, as per the input.
+    Raises:
+        scale or lam type error.
+    """
+    if isinstance(scale, (int, float)):
+        new_scale = scale
+    else:
+        assert isinstance(scale, (tuple, list)), 'scale type should be one of int, float, tuple, list.'
+        new_scale = tf.random.uniform([], scale[0], scale[1], seed=seed)
     image = tf.cast(image, dtype=tf.float32)
-    image_shape = image.get_shape().as_list()
+    image_shape = tf.cast(_ImageDimensions(image, image.get_shape().ndims), dtype=tf.int32)
     if isinstance(lam, (int, float)):
-        image = tf.math.add(tf.math.multiply(tf.random.poisson(lam, image_shape, seed=seed), scale), image)
+        image = tf.math.add(tf.math.multiply(tf.random.poisson(lam, image_shape, seed=seed), new_scale), image)
     elif isinstance(lam, (tuple, list)):
         random_lam = tf.random.uniform([], lam[0], lam[1])
-        image = tf.math.add(tf.math.multiply(tf.random.poisson(random_lam, image_shape, seed=seed), scale), image)
+        image = tf.math.add(tf.math.multiply(tf.random.poisson(random_lam, image_shape, seed=seed), new_scale), image)
     else:
         raise ValueError('lam type should be one of int, float, tuple, list.')
     return image
 
 def RandomNoiseMask(image, keep_prob=0.95, seed=None):
+    """Mask noise apply to image.
+    
+    With probability `keep_prob`, outputs the input element scaled up by
+    `1`, otherwise outputs `0`. 
+    Args:
+        image: Either a 3-D float Tensor of shape [height, width, depth], or a 4-D
+               Tensor of shape [batch_size, height, width, depth].
+        keep_prob: should be in the interval (0, 1].
+                   if int or float, the probability that each element is kept.
+                   if tuple or list, randomly picked in the interval
+                   `[keep_prob[0], keep_prob[1])`, the probability that each element is kept.
+        seed: A Python integer. Used to create a random seed.
+              See `tf.set_random_seed` for behavior.
+    Returns:
+        3-D / 4-D float Tensor, as per the input.
+    Raises:
+        ValueError: If `keep_prob` is not in `(0, 1]`.
+    """
+    image = tf.cast(image, dtype=tf.float32)
+    image_shape = tf.cast(_ImageDimensions(image, image.get_shape().ndims), dtype=tf.int32)
     if isinstance(keep_prob, float):
         mask = tf.clip_by_value(tf.nn.dropout(tf.random.uniform(image_shape, 1., 2.), keep_prob), 0., 1.)
         image = tf.math.multiply(mask, image)
@@ -493,26 +588,72 @@ def RandomNoiseMask(image, keep_prob=0.95, seed=None):
     return image
 
 def RandomNoiseSaltPepper(image, keep_prob=0.8, seed=None):
-    assert isinstance(keep_prob, (int, float)), 'keep_prob type should be one of int, float.'
+    """Salt-Pepper noise apply to image.
+    
+    The salt-pepper noise is based on the signal-to-noise ratio of the image,
+    randomly generating the pixel positions in some images all channel,
+    and randomly assigning these pixels to 0 or 255.
+    Args:
+        image: Either a 3-D float Tensor of shape [height, width, depth], or a 4-D
+               Tensor of shape [batch_size, height, width, depth].
+        keep_prob: should be in the interval (0, 1].
+                   if int or float, the probability that each element is kept.
+                   if tuple or list, randomly picked in the interval
+                   `[keep_prob[0], keep_prob[1])`, the probability that each element is kept.
+        seed: A Python integer. Used to create a random seed.
+              See `tf.set_random_seed` for behavior.
+    Returns:
+        3-D / 4-D float Tensor, as per the input.
+    Raises:
+        ValueError: If `keep_prob` is not in `(0, 1]`.
+    """
     image = tf.cast(image, dtype=tf.float32)
-    image_shape = image.get_shape().as_list()
+    if image.get_shape().ndims==4:
+        shape = tf.cast(_ImageDimensions(image, 4), dtype=tf.int32)
+        image_shape = [shape[0], shape[1], shape[2], 1]
+    else:
+        shape = tf.cast(_ImageDimensions(image, image.get_shape().ndims), dtype=tf.int32)
+        image_shape = [shape[0], shape[1], 1]
     if isinstance(keep_prob, (int, float)):
         noise = tf.clip_by_value(tf.math.floor(tf.random.uniform(image_shape, 0.5-0.5/keep_prob, 0.5+0.5/keep_prob)), -1, 1)
         image = tf.clip_by_value(tf.math.add(tf.math.multiply(noise, 255.), image), 0, 255)
     elif isinstance(keep_prob, (tuple, list)):
         random_keep_prob = tf.random.uniform([], keep_prob[0], keep_prob[1], seed=seed)
-        noise = tf.clip_by_value(tf.math.floor(tf.random.uniform(image_shape, 0.5-0.5/keep_prob, 0.5+0.5/keep_prob)), -1, 1)
+        noise = tf.clip_by_value(tf.math.floor(tf.random.uniform(image_shape, 0.5-0.5/random_keep_prob, 0.5+0.5/random_keep_prob)), -1, 1)
         image = tf.clip_by_value(tf.math.add(tf.math.multiply(noise, 255.), image), 0, 255)
     else:
         raise ValueError('keep_prob type should be one of int, float, tuple, list.')
     return image
 
-def RandomRescale(image, scale, seed=None):
-    if isinstance(scale, (int, float)):
-        image = tf.math.multiply(image, scale)
-    elif isinstance(scale, (tuple, list)):
-        random_scale = tf.random.uniform([], scale[0], scale[1], seed=seed)
-        image = tf.math.multiply(image, random_scale)
+def RandomNoiseRainbow(image, keep_prob=0.8, seed=None):
+    """Rainbowr noise apply to image.
+    
+    The rainbow noise is based on the signal-to-noise ratio of the image,
+    randomly generating the pixel positions in some images,
+    and randomly assigning these pixels to 0 or 255.
+    Args:
+        image: Either a 3-D float Tensor of shape [height, width, depth], or a 4-D
+               Tensor of shape [batch_size, height, width, depth].
+        keep_prob: should be in the interval (0, 1].
+                   if int or float, the probability that each element is kept.
+                   if tuple or list, randomly picked in the interval
+                   `[keep_prob[0], keep_prob[1])`, the probability that each element is kept.
+        seed: A Python integer. Used to create a random seed.
+              See `tf.set_random_seed` for behavior.
+    Returns:
+        3-D / 4-D float Tensor, as per the input.
+    Raises:
+        ValueError: If `keep_prob` is not in `(0, 1]`.
+    """
+    image = tf.cast(image, dtype=tf.float32)
+    image_shape = tf.cast(_ImageDimensions(image, image.get_shape().ndims), dtype=tf.int32)
+    if isinstance(keep_prob, (int, float)):
+        noise = tf.clip_by_value(tf.math.floor(tf.random.uniform(image_shape, 0.5-0.5/keep_prob, 0.5+0.5/keep_prob)), -1, 1)
+        image = tf.clip_by_value(tf.math.add(tf.math.multiply(noise, 255.), image), 0, 255)
+    elif isinstance(keep_prob, (tuple, list)):
+        random_keep_prob = tf.random.uniform([], keep_prob[0], keep_prob[1], seed=seed)
+        noise = tf.clip_by_value(tf.math.floor(tf.random.uniform(image_shape, 0.5-0.5/random_keep_prob, 0.5+0.5/random_keep_prob)), -1, 1)
+        image = tf.clip_by_value(tf.math.add(tf.math.multiply(noise, 255.), image), 0, 255)
     else:
-        raise ValueError('scale type should be one of int, float, tuple, list.')
+        raise ValueError('keep_prob type should be one of int, float, tuple, list.')
     return image
