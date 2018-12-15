3438728f050fc9a13e41d3a1ae132851bcce0ecd
JinQing Lee <hourout@163.com>
2018-12-15 21:58:47 +0800

    Update __init__.py

--- tensorcv/data/datasets/__init__.py
+++ tensorcv/data/datasets/__init__.py
@@ -1,1 +1,6 @@
+from tensorcv.data.datasets._mnist import mnist
+
+from tensorcv.data.datasets._mnist import mnist_fashion
+
 from tensorcv.data.datasets._mnist_kuzushiji import mnist_kuzushiji10
+from tensorcv.data.datasets._mnist_kuzushiji import mnist_kuzushiji49
