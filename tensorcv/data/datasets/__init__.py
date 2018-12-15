3f84b179c2de8178a5b9c1b16e60d2cacbc7cc86
JinQing Lee <hourout@163.com>
2018-12-15 23:58:07 +0800

    Update __init__.py

--- tensorcv/data/datasets/__init__.py
+++ tensorcv/data/datasets/__init__.py
@@ -1,5 +1,7 @@
-from tensorcv.data.datasets._mnist import mnist
+from tensorcv.data.datasets._cifar import cifar10
+from tensorcv.data.datasets._cifar import cifar100
 
+from tensorcv.data.datasets._mnist import mnist
 from tensorcv.data.datasets._mnist import mnist_fashion
 
 from tensorcv.data.datasets._mnist_kuzushiji import mnist_kuzushiji10
