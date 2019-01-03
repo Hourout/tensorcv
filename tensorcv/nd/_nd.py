2ac2c471b21b0071147c18ffe05c620dee569045
JinQing Lee <hourout@163.com>
2019-01-03 17:28:11 +0800

    Update _nd.py

--- tensorcv/nd/_nd.py
+++ tensorcv/nd/_nd.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-__all__ = ['softshrink']
+__all__ = ['softshrink', 'tanhshrink', 'softmin', 'log_softmin']
 
 def softshrink(x, delta=0.5, name=None):
     """Applies the soft shrinkage function elementwise:
@@ -21,3 +21,45 @@ def softshrink(x, delta=0.5, name=None):
     return tf.case({tf.less(x, -delta): f1, 
                     tf.greater(x, delta): f2},
                    default=f3, exclusive=True)
+
+def tanhshrink(x, name=None):
+    """Applies the tanh shrinkage function elementwise:
+    
+    output = x - tanh(x)
+    Args:
+        x: A Tensor with type float32 or float64.
+        name: A name for the operation (optional).
+    Returns:
+        A Tensor with the same type as x.
+    """
+    return tf.math.subtract(x, tf.math.tanh(x))
+
+def softmin(x, axis=None, name=None):
+    """Applies the Softmin function to an n-dimensional 
+    input Tensor rescaling them so that the elements of 
+    the n-dimensional output Tensor lie in the range (0, 1) and sum to 1
+    
+    output = exp(-x)/sum(exp(-x))
+    Args:
+        x: A Tensor with type float32 or float64.
+        axis: The dimension softmax would be performed on. The default is -1 which indicates the last dimension.
+        name: A name for the operation (optional).
+    Returns:
+        A Tensor with the same type as x.
+    """
+    return tf.exp(-x) / tf.reduce_sum(tf.exp(-x), axis)
+
+def log_softmin(x, axis=None, name=None):
+    """Applies the log Softmin function to an n-dimensional 
+    input Tensor rescaling them so that the elements of 
+    the n-dimensional output Tensor lie in the range (0, 1) and sum to 1
+    
+    output = log(exp(-x)/sum(exp(-x)))
+    Args:
+        x: A Tensor with type float32 or float64.
+        axis: The dimension softmax would be performed on. The default is -1 which indicates the last dimension.
+        name: A name for the operation (optional).
+    Returns:
+        A Tensor with the same type as x.
+    """
+    return tf.math.log(tf.exp(-x) / tf.reduce_sum(tf.exp(-x), axis))
